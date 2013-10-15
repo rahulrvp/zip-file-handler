84e82260a1dc1fde6af6e6578f0a596267bbb854
Rahul <rahul@bullfin.ch>
2013-10-15 15:16:47 +0530

    made singleton and added crash checkings

--- src/ch/bullfin/android/ZipFileHandler/ZipFileHandler.java
+++ src/ch/bullfin/android/ZipFileHandler/ZipFileHandler.java
@@ -2,13 +2,12 @@ package ch.bullfin.android.ZipFileHandler;
 
 import android.util.Log;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,21 +18,72 @@ import java.util.zip.ZipInputStream;
 public class ZipFileHandler {
     private static final String LOG_TAG = "ZipFileHandler";
 
+    private ArrayList<File> inputFiles = null;
     private File inputFile = null;
     private URL inputUrl = null;
     private String outputPath = null;
 
-    public ZipFileHandler(URL inputUrl, String outputPath) {
-        this.inputUrl = inputUrl;
-        this.outputPath = outputPath;
+    private ZipFileHandler() {}
+    private static ZipFileHandler instance = null;
+    public static ZipFileHandler getInstance() {
+        if (instance == null) {
+            instance = new ZipFileHandler();
+        }
+        return instance;
     }
 
-    public ZipFileHandler(File inputFile, String outputPath) {
-        this.inputFile = inputFile;
+    public void unZip(URL inputUrl, String outputPath) {
+        this.inputUrl   = inputUrl;
         this.outputPath = outputPath;
+        unZip();
     }
 
-    public void unZip() {
+    public void unZip(File inputFile, String outputPath) {
+        this.inputFile  = inputFile;
+        this.outputPath = outputPath;
+        unZip();
+    }
+
+    public void zip(ArrayList<File> inputFiles, String outputPath) {
+        this.inputFiles = inputFiles;
+        this.outputPath = outputPath;
+        File outputFile = new File(outputPath);
+        if (!outputFile.exists()){
+            try {
+                if (!outputFile.createNewFile()) { return; }
+            } catch (IOException e) { Log.v(LOG_TAG, e.getMessage()); }
+        }
+        zip();
+    }
+
+    private void zip() {
+        if (inputFiles != null && outputPath != null) {
+            try {
+                BufferedInputStream bufferedInputStream;
+                FileOutputStream zipFileOutputStream = new FileOutputStream(outputPath);
+                ZipOutputStream zipOutputStream = new ZipOutputStream(new BufferedOutputStream(zipFileOutputStream));
+                byte[] buffer = new byte[1024];
+                for (File inputFile : inputFiles) {
+                    ZipEntry zipEntry = new ZipEntry(inputFile.getName());
+                    zipOutputStream.putNextEntry(zipEntry);
+
+                    FileInputStream fileInputStream = new FileInputStream(inputFile);
+                    bufferedInputStream = new BufferedInputStream(fileInputStream, 1024);
+                    int length;
+                    while ((length = bufferedInputStream.read(buffer)) != -1) {
+                        zipOutputStream.write(buffer, 0, length);
+                    }
+                }
+                zipOutputStream.close();
+            } catch (FileNotFoundException e) {
+                Log.v(LOG_TAG, e.getMessage());
+            } catch (IOException e) {
+                Log.v(LOG_TAG, e.getMessage());
+            }
+        }
+    }
+
+    private void unZip() {
         ZipInputStream zipInputStream = null;
             try {
                 if (inputUrl != null) {
@@ -41,7 +91,9 @@ public class ZipFileHandler {
                 } else if (inputFile != null) {
                     zipInputStream = new ZipInputStream(new FileInputStream(inputFile));
                 }
-            } catch (IOException e) { e.printStackTrace(); }
+            } catch (IOException e) {
+                Log.v(LOG_TAG, e.getMessage());
+            }
 
         if (zipInputStream != null){
             unZip(zipInputStream);
@@ -55,7 +107,7 @@ public class ZipFileHandler {
                 if (zipEntry.isDirectory()) {
                     addDirectory(outputPath + File.separator + zipEntry.getName());
                 } else {
-                    FileOutputStream fileOutputStream = new FileOutputStream(outputPath);
+                    FileOutputStream fileOutputStream = new FileOutputStream(outputPath + File.separator + zipEntry.getName());
                     byte[] buffer = new byte[1024];
                     int length;
                     while ((length = zipInputStream.read(buffer)) != -1){
--- src/ch/bullfin/android/ZipFileHandler/ZipFileHandlerActivity.java
+++ src/ch/bullfin/android/ZipFileHandler/ZipFileHandlerActivity.java
@@ -2,6 +2,13 @@ package ch.bullfin.android.ZipFileHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,7 +17,33 @@ import android.os.Bundle;
  * Time: 1:26 PM
  */
 public class ZipFileHandlerActivity extends Activity {
+    private static final String DOWNLOAD_URL = "https://dl.dropboxusercontent.com/u/72084171/sample.zip";
+    private static final String SD_CARD_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.zip_file_handler_home);
+    }
+
+    public void unzipFromUrl(View view) {
+        try {
+            URL url = new URL(DOWNLOAD_URL);
+            ZipFileHandler.getInstance().unZip(url, SD_CARD_PATH);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void unzipFromFile(View view) {
+        ZipFileHandler.getInstance().unZip(new File(SD_CARD_PATH + "/sample.zip"), SD_CARD_PATH);
+    }
+
+    public void zipFilesIntoOne(View view) {
+        ArrayList<File> inputFiles = new ArrayList<File>();
+        inputFiles.add(new File(SD_CARD_PATH + File.separator + "sample_1.txt"));
+        inputFiles.add(new File(SD_CARD_PATH + File.separator + "sample_2.txt"));
+        inputFiles.add(new File(SD_CARD_PATH + File.separator + "sample_3.txt"));
+        ZipFileHandler.getInstance().zip(inputFiles, SD_CARD_PATH + "/sample.zip");
     }
 }
